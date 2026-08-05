9fcb54c58fb824d239412f08bf6a425887e088f9
thanhdt05 <226494707+thanhdt05@users.noreply.github.com>
2026-08-05 11:26:13 +0700

    [T1.5-T1.9][thanhdt] Implement RBAC schema, roles, permissions seeder and initial admin user

---
+++ app/Models/Role.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'display_name',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_permissions');
+    }
+}
